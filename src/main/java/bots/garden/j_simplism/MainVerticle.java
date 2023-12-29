24c1f70e1e0d05831485bdba112402e4048933f0
@k33g <ph.charriere@gmail.com>
2023-12-29 07:18:27 +0100

    first commit

---
+++ src/main/java/bots/garden/j_simplism/MainVerticle.java
@@ -0,0 +1,55 @@
+package bots.garden.j_simplism;
+
+import com.dylibso.chicory.runtime.Memory;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+import com.dylibso.chicory.runtime.ExportFunction;
+import com.dylibso.chicory.wasm.types.Value;
+import com.dylibso.chicory.runtime.Module;
+import com.dylibso.chicory.runtime.Instance;
+
+import java.io.File;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    System.out.println("📦 Loading wasm file...");
+
+    File wasmFile = new File("./hello.wasm");
+    Module module = Module.build(wasmFile);
+    Instance instance = module.instantiate();
+
+    ExportFunction helloFunction = instance.getExport("hello");
+    // https://github.com/dylibso/chicory?tab=readme-ov-file#memory-and-complex-types
+    ExportFunction alloc = instance.getExport("alloc");
+    ExportFunction dealloc = instance.getExport("dealloc");
+
+    vertx.createHttpServer().requestHandler(req -> {
+
+      Memory memory = instance.getMemory();
+      String message = "Bob Morane";
+      int len = message.getBytes().length;
+      // allocate {len} bytes of memory, this returns a pointer to that memory
+      int ptr = alloc.apply(Value.i32(len))[0].asInt();
+      // We can now write the message to the module's memory:
+      memory.put(ptr, message);
+
+      Value result = helloFunction.apply(Value.i32(ptr), Value.i32(len))[0];
+      dealloc.apply(Value.i32(ptr), Value.i32(len));
+
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end(result.toString());
+
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
